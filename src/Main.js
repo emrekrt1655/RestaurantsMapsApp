cb6321f264ea545cb2543e2fc934897d0c5707ba
Emre <emrekurtt1655@gmail.com>
2020-11-08 01:22:50 +0100

    add: city search implemented

--- src/Main.js
+++ src/Main.js
@@ -5,38 +5,55 @@ import {floatBanner} from './styles'
 import {City, RestaurantDetail, SearchBar} from './components';
 import Axios from 'axios';
 
+
+let originalList = [];
+
 const Main = (props) => {
   const [cityList, setCityList] = useState([])
 
   const fetchCities = async () => {
-   const {data} = await Axios.get("https://opentable.herokuapp.com/api/cities");
-   setCityList(data.cities)
+    const {data} = await Axios.get("https://opentable.herokuapp.com/api/cities");
+    setCityList(data.cities);
+    originalList = [ ...data.cities];
   }
 
   useEffect(()=>{fetchCities()},[])
+
+  const onCitySearch = (text) =>{
+    const filteredList = originalList.filter(city => {
+      const userText = text.toUpperCase();
+      const cityName = city.toUpperCase();
+
+      return(
+        cityName.indexOf(userText) > -1
+      );
+    })
+    setCityList(filteredList)
+  };
 
 
   return (
     <SafeAreaView style={{flex: 1}}>
        
+
       <MapView
-      style = {{flex:1}}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
-  <View style={floatBanner.container}>
-    <SearchBar/>
-    <FlatList
-    horizontal
-      keyExtractor = {(_, index) => index.toString()}
-      data={cityList}
-      renderItem = { ({item})=> <City cityName={item} />}
+        style = {{flex:1}}
+        initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
     />
-    </View>
+      <View style={floatBanner.container}>
+        <SearchBar onSearch={ onCitySearch  }/>
+        <FlatList
+          horizontal
+          keyExtractor = {(_, index) => index.toString()}
+          data={cityList}
+          renderItem = { ({item})=> <City cityName={item} />}
+      />
+      </View>
       
 
     </SafeAreaView>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,7 @@ const SearchBar = (props) => {
       <TextInput
         style={searchbarStyle.input}
         placeholder='Search a city'
+        onChangeText={value => props.onSearch(value)}
       />
     </View>
   );
